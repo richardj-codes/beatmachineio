f605103916379450bd6d610c184c041fd67a3f88
Richard <74265663+romeorpj@users.noreply.github.com>
2022-05-13 09:47:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -25,39 +25,39 @@ document.addEventListener("keydown", (event) => {
 	// keyBoardSoundz(event.key);
 	switch (event.key) {
 		case "q":
-			let sound1 = new Audio("/sounds/RD_C_7.wav");
+			let sound1 = new Audio("sounds/RD_C_7.wav");
 			sound1.play();
 			break;
 		case "w":
-			let sound2 = new Audio("/sounds/RD_C_HH_4.wav");
+			let sound2 = new Audio("sounds/RD_C_HH_4.wav");
 			sound2.play();
 			break;
 		case "e":
-			let sound3 = new Audio("/sounds/RD_K_5.wav");
+			let sound3 = new Audio("sounds/RD_K_5.wav");
 			sound3.play();
 			break;
 		case "a":
-			let sound4 = new Audio("/sounds/RD_P_SB_1.wav");
+			let sound4 = new Audio("sounds/RD_P_SB_1.wav");
 			sound4.play();
 			break;
 		case "s":
-			let sound5 = new Audio("/sounds/RD_S_P_5.wav");
+			let sound5 = new Audio("sounds/RD_S_P_5.wav");
 			sound5.play();
 			break;
 		case "d":
-			let sound6 = new Audio("/sounds/RD_T_FT_2.wav");
+			let sound6 = new Audio("sounds/RD_T_FT_2.wav");
 			sound6.play();
 			break;
 		case "z":
-			let sound7 = new Audio("/sounds/RD_T_HT_1.wav");
+			let sound7 = new Audio("sounds/RD_T_HT_1.wav");
 			sound7.play();
 			break;
 		case "x":
-			let sound8 = new Audio("/sounds/RD_T_HT_7.wav");
+			let sound8 = new Audio("sounds/RD_T_HT_7.wav");
 			sound8.play();
 			break;
 		case "c":
-			let sound9 = new Audio("/sounds/RD_T_MT_1.wav");
+			let sound9 = new Audio("sounds/RD_T_MT_1.wav");
 			sound9.play();
 			break;
 
@@ -70,39 +70,39 @@ document.addEventListener("keydown", (event) => {
 let keyBoardSoundz = (clicks) => {
 	switch (clicks) {
 		case q:
-			let sound1 = new Audio("/sounds/RD_C_7.wav");
+			let sound1 = new Audio("sounds/RD_C_7.wav");
 			sound1.play();
 			break;
 		case w:
-			let sound2 = new Audio("/sounds/RD_C_HH_4.wav");
+			let sound2 = new Audio("sounds/RD_C_HH_4.wav");
 			sound2.play();
 			break;
 		case e:
-			let sound3 = new Audio("/sounds/RD_K_5.wav");
+			let sound3 = new Audio("sounds/RD_K_5.wav");
 			sound3.play();
 			break;
 		case a:
-			let sound4 = new Audio("/sounds/RD_P_SB_1.wav");
+			let sound4 = new Audio("sounds/RD_P_SB_1.wav");
 			sound4.play();
 			break;
 		case s:
-			let sound5 = new Audio("/sounds/RD_S_P_5.wav");
+			let sound5 = new Audio("sounds/RD_S_P_5.wav");
 			sound5.play();
 			break;
 		case d:
-			let sound6 = new Audio("/sounds/RD_T_FT_2.wav");
+			let sound6 = new Audio("sounds/RD_T_FT_2.wav");
 			sound6.play();
 			break;
 		case z:
-			let sound7 = new Audio("/sounds/RD_T_HT_1.wav");
+			let sound7 = new Audio("sounds/RD_T_HT_1.wav");
 			sound7.play();
 			break;
 		case x:
-			let sound8 = new Audio("/sounds/RD_T_HT_7.wav");
+			let sound8 = new Audio("sounds/RD_T_HT_7.wav");
 			sound8.play();
 			break;
 		case c:
-			let sound9 = new Audio("/sounds/RD_T_MT_1.wav");
+			let sound9 = new Audio("sounds/RD_T_MT_1.wav");
 			sound9.play();
 			break;
 
